39458220bc36e581c7b3355b43bd35a8c0e23c8a
CyanZee <hyy_swallow@outlook.com>
2018-04-12 09:25:22 +0800

    Release version. Add showProc_etime and stop function.

--- shell.sh
+++ shell.sh
@@ -11,9 +11,36 @@ getReleaseTime_func()
 
 getReleaseTime_func
 
+showProc_etime_func()
+{
+    ps -p $1 -o etime
+}
 
+stop_func()
+{
+    procs=`ps -ef | grep $1 | grep -v 'grep\run'| awk '{print $2}'`
+    for pid in ${procs}
+    do
+        echo ${pid}
+        sudo kill -9 ${pid}
+    done
+}
 
 case $1 in
-	*) echo "unknow command."
-	;;
+    "pe") echo "Showing process run time."
+        showProc_etime_func $2
+        ;;
+    "stop") echo "Stopping process..."
+        stop_func $2
+        ;;
+    "help") echo "Show process run time."
+        echo "Try './shell.sh pe [PID]'"
+        echo ""
+        echo "Stop process by its part name."
+        echo "Try './shell.sh stop [ProcName]' eg:./shell.sh stop goStreamerSrv"
+        echo ""
+        ;;
+     *) echo "unknow command."
+        echo "Try './shell.sh help' for more information."
+        ;;
 esac
--- version/version.go
+++ version/version.go
@@ -2,5 +2,5 @@ package version
 
 const (
 	Version = "1.0"
-	ReleaseTime = "2018-04-11 13:26:07 CST"
+	ReleaseTime = "2018-04-12 09:21:21 CST"
 )
